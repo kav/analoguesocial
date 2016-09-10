cbd134c2a055e80dd0cc8b935ed82e15a106d12a
Sergio Prada <chechopradaster@gmail.com>
2016-09-10 16:05:07 -0400

    Add twilio route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import describeImage from './describe-image';
+import sendSMS from './twilio-sms';
+
 describeImage();
 const app = express();
 
@@ -10,6 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('./static'));
+
+app.use('/message', sendSMS);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
---
+++ server/twilio-sms.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+import fetch from 'node-fetch';
+import twilio from 'twilio';
+
+const router = express.Router();
+const client = twilio('AC941d110142d546bc70970e028751bbd2', 'd5d30b74960c418bace9405d07611915');
+
+const sendSMS = (number, payload, cb) => {
+  client.sendMessage({
+    to: '+1' + number,
+    from: '+12025172625',
+    body: payload
+  }, function(err, responseData) { //this function is executed when a response is received from Twilio
+      if (!err) { // "err" is an error received during the request, if any
+          console.log(responseData.from); // outputs "+14506667788"
+          console.log(responseData.body); // outputs "word to your mother."
+      }
+      cb(err);
+  });
+};
+
+router.post('/:number', function(req, res, next){
+  sendSMS(req.params.number, req.body.message, function(err){
+    if(err) res.status(400).send(err);
+    res.send();
+  })
+});
+
+export default router;
