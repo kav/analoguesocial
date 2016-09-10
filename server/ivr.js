9849ecaa96e58b7fb1728dc64a99a0c2063132a9
Kavanaugh Latiolais <kav@liffft.com>
2016-09-10 16:27:20 -0400

    stuff

--- server/ivr.js
+++ server/ivr.js
@@ -1,7 +1,44 @@
 import express from 'express';
-const twilio = require('twilio');
+import twilio from 'twilio';
 
+// eslint-disable-next-line new-cap
 const router = express.Router();
+
+const giveExtractionPointInstructions = (twiml) => {
+  twiml.say('To get to your extraction point, get on your bike and go down ' +
+        'the street. Then Left down an alley. Avoid the police cars. Turn left ' +
+        'into an unfinished housing development. Fly over the roadblock. Go ' +
+        'passed the moon. Soon after you will see your mother ship.',
+        { voice: 'alice', language: 'en-GB' });
+
+  twiml.say('Thank you for calling the ET Phone Home Service - the ' +
+        "adventurous alien's first choice in intergalactic travel");
+
+  twiml.hangup();
+  return twiml;
+};
+
+const listPlanets = (twiml) => {
+  twiml.gather({
+    action: '/ivr/planets',
+    numDigits: '1',
+    method: 'POST',
+  }, (node) => {
+    node.say('To call the planet Broh doe As O G, press 2. To call the planet ' +
+            'DuhGo bah, press 3. To call an oober asteroid to your location, press 4. To ' +
+            'go back to the main menu, press the star key ',
+            { voice: 'alice', language: 'en-GB', loop: 3 });
+  });
+  return twiml;
+};
+
+const redirectWelcome = () => {
+  const twiml = new twilio.TwimlResponse();
+  twiml.say('Returning to the main menu', { voice: 'alice', language: 'en-GB' });
+  twiml.redirect('/ivr/welcome');
+  return twiml;
+};
+
 
 // POST: '/ivr/welcome'
 router.post('/welcome', twilio.webhook({ validate: false }), (request, response) => {
@@ -20,8 +57,8 @@ router.post('/welcome', twilio.webhook({ validate: false }), (request, response)
 router.post('/menu', twilio.webhook({ validate: false }), (request, response) => {
   const selectedOption = request.body.Digits;
   const optionActions = {
-    '1': giveExtractionPointInstructions,
-    '2': listPlanets,
+    1: giveExtractionPointInstructions,
+    2: listPlanets,
   };
 
   if (optionActions[selectedOption]) {
@@ -36,9 +73,9 @@ router.post('/menu', twilio.webhook({ validate: false }), (request, response) =>
 router.post('/planets', twilio.webhook({ validate: false }), (request, response) => {
   const selectedOption = request.body.Digits;
   const optionActions = {
-    '2': '+12024173378',
-    '3': '+12027336386',
-    '4': '+12027336637',
+    2: '+12024173378',
+    3: '+12027336386',
+    4: '+12027336637',
   };
 
   if (optionActions[selectedOption]) {
@@ -49,40 +86,4 @@ router.post('/planets', twilio.webhook({ validate: false }), (request, response)
   response.send(redirectWelcome());
 });
 
-let giveExtractionPointInstructions = function (twiml) {
-  twiml.say('To get to your extraction point, get on your bike and go down ' +
-        'the street. Then Left down an alley. Avoid the police cars. Turn left ' +
-        'into an unfinished housing development. Fly over the roadblock. Go ' +
-        'passed the moon. Soon after you will see your mother ship.',
-        { voice: 'alice', language: 'en-GB' });
-
-  twiml.say('Thank you for calling the ET Phone Home Service - the ' +
-        "adventurous alien's first choice in intergalactic travel");
-
-  twiml.hangup();
-  return twiml;
-};
-
-let listPlanets = function (twiml) {
-  twiml.gather({
-    action: '/ivr/planets',
-    numDigits: '1',
-    method: 'POST',
-  }, (node) => {
-    node.say('To call the planet Broh doe As O G, press 2. To call the planet ' +
-            'DuhGo bah, press 3. To call an oober asteroid to your location, press 4. To ' +
-            'go back to the main menu, press the star key ',
-            { voice: 'alice', language: 'en-GB', loop: 3 });
-  });
-  return twiml;
-};
-
-let redirectWelcome = function () {
-  const twiml = new twilio.TwimlResponse();
-  twiml.say('Returning to the main menu', { voice: 'alice', language: 'en-GB' });
-  twiml.redirect('/ivr/welcome');
-  return twiml;
-};
-
-
-module.exports = router;
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 
