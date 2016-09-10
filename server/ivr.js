1b5f226d97f8e0724dd71b0a043e925d13148bf6
Kavanaugh Latiolais <kav@liffft.com>
2016-09-10 17:31:48 -0400

    ivr work

--- server/ivr.js
+++ server/ivr.js
@@ -4,33 +4,22 @@ import twilio from 'twilio';
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-const giveExtractionPointInstructions = (twiml) => {
-  twiml.say('To get to your extraction point, get on your bike and go down ' +
-        'the street. Then Left down an alley. Avoid the police cars. Turn left ' +
-        'into an unfinished housing development. Fly over the roadblock. Go ' +
-        'passed the moon. Soon after you will see your mother ship.',
-        { voice: 'alice', language: 'en-GB' });
-
-  twiml.say('Thank you for calling the ET Phone Home Service - the ' +
-        "adventurous alien's first choice in intergalactic travel");
-
-  twiml.hangup();
-  return twiml;
-};
-
-const listPlanets = (twiml) => {
+const viewInstagramPost = (twiml) => {
   twiml.gather({
     action: '/ivr/planets',
     numDigits: '1',
     method: 'POST',
   }, (node) => {
-    node.say('To call the planet Broh doe As O G, press 2. To call the planet ' +
-            'DuhGo bah, press 3. To call an oober asteroid to your location, press 4. To ' +
-            'go back to the main menu, press the star key ',
-            { voice: 'alice', language: 'en-GB', loop: 3 });
+    node.say('At LOCATION on DATE at TIME, USERNAME took a photo of DESCRIPTION'
+    + 'To like this photo, please press 1.'
+    + 'To comment on this photo, please press 2.'
+    + 'To share this photo, please press 3'
+    + 'To view next photo, please press 4',
+    { voice: 'alice', language: 'en-GB' });
   });
-  return twiml;
 };
+
+//   twiml.hangup();
 
 const redirectWelcome = () => {
   const twiml = new twilio.TwimlResponse();
@@ -48,7 +37,12 @@ router.post('/welcome', twilio.webhook({ validate: false }), (request, response)
     numDigits: '1',
     method: 'POST',
   }, (node) => {
-    node.play('http://howtodocs.s3.amazonaws.com/et-phone.mp3', { loop: 3 });
+    node.say('Welcome to Instagram, Capture and Share the World\'s Moments,' +
+      'Brought to you by Analogue Social;' +
+      'surfing the information superhighway at the pace of yesterday' +
+      'To view your Instagram feed please press 1.' +
+      'To repeat this message please stay on the line'
+      , { loop: 3 });
   });
   response.send(twiml);
 });
@@ -57,8 +51,7 @@ router.post('/welcome', twilio.webhook({ validate: false }), (request, response)
 router.post('/menu', twilio.webhook({ validate: false }), (request, response) => {
   const selectedOption = request.body.Digits;
   const optionActions = {
-    1: giveExtractionPointInstructions,
-    2: listPlanets,
+    1: viewInstagramPost,
   };
 
   if (optionActions[selectedOption]) {
@@ -69,13 +62,13 @@ router.post('/menu', twilio.webhook({ validate: false }), (request, response) =>
   response.send(redirectWelcome());
 });
 
-// POST: '/ivr/planets'
-router.post('/planets', twilio.webhook({ validate: false }), (request, response) => {
+// POST: '/ivr/instagram_actions'
+router.post('/instagram_actions', twilio.webhook({ validate: false }), (request, response) => {
   const selectedOption = request.body.Digits;
   const optionActions = {
-    2: '+12024173378',
-    3: '+12027336386',
-    4: '+12027336637',
+    // 1: like,
+    // 2: comment,
+    // 3: share,
   };
 
   if (optionActions[selectedOption]) {
