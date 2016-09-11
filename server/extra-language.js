61d22dd33f0e4da6bdf43168e9db5f5d0a097202
thomas henderson <tom@mathpunk.net>
2016-09-11 13:12:23 -0400

    Add text content for audiogram filters

---
+++ server/extra-language.js
@@ -0,0 +1,11 @@
+// Audio Filters
+
+const filters = {
+  'jean-luc': {'voice': 'man',   // I made this up
+               'language': 'fr',
+               'closingDialogue': 'Magnifique.'},
+  'nashville': {'closingDialogue': 'What memories'},
+  'valencia': {'closingDialogue': 'like the ones from my childhood'},
+  'lo-fi': {'closingDialogue': 'Without shadow there can be no light'},
+};
+
