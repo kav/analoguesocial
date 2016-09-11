79cb597e5e9f97330b3acf77de0aa17c2fe98f70
Sergio Prada <chechopradaster@gmail.com>
2016-09-10 20:42:12 -0400

    Add telephone query param

--- server/sign-up.js
+++ server/sign-up.js
@@ -7,9 +7,9 @@ import Firebase from 'firebase';
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-router.get('/', (req, res, ) => {
+router.post('/', (req, res, ) => {
   console.log('REQ MADE');
-  const envUrl = `http://${req.headers.host}/signup/token`;
+  const envUrl = `http://${req.headers.host}/signup/token?tel=${req.body.number}`;
   res.redirect('https://api.instagram.com/oauth/authorize/' +
   '?client_id=9b6c05b9a31643ea9abcd7651f7a6bd2' +
   '&scope=follower_list+likes+comments' +
@@ -19,13 +19,14 @@ router.get('/', (req, res, ) => {
 
 router.get('/token', (req, res) => {
   const code = req.query.code;
+  const tel = req.query.tel;
 
   const body = new FormData();
   body.append('client_id', '9b6c05b9a31643ea9abcd7651f7a6bd2');
   body.append('client_secret', 'dc67648b48f3412b92a02e6bd817b68f');
   body.append('code', code);
   body.append('grant_type', 'authorization_code');
-  body.append('redirect_uri', `http://${req.headers.host}/signup/token`);
+  body.append('redirect_uri', `http://${req.headers.host}/signup/token?tel=${tel}`);
 
   fetch('https://api.instagram.com/oauth/access_token', {
     method: 'POST',
