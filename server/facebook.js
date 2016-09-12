1fd1f4e70bbce26f1e4db131351544bc54969250
Kavanaugh Latiolais <kav@liffft.com>
2016-09-11 22:41:08 -0400

    fb data structure

--- server/facebook.js
+++ server/facebook.js
@@ -34,14 +34,12 @@ export const precacheFbPosts = (phone, token) => {
   fetch(`https://graph.facebook.com/me/feed?limit=10&access_token=${token}`)
   .then((res) => res.json())
   .then((json) => {
-    console.log(json);
-    json.forEach((fbPost) => {
+    json.data.forEach((fbPost) => {
       const post = {
         description: `${fbPost.story} ${fbPost.message ? `with message; ${fbPost.message}` : ''}`,
       };
-      console.log(`Saving: ${post}`);
       postsRef.child(phone).push(post);
     });
   });
 };
-// precacheFbPosts('+12063312167','EAACe39enYZCoBAOAZCZAa3puW6OpRr10kcBbSVKhUJnFQyCDDpz0hm1I010zsiLJ0uJIYtIFxdkAylr0BFqgPHxDvNvNqRoKM7ZCB1C86RoZAwlZAnZBs4yNUfo3dbQirDuJjhSBhO9BWQbvzFHPufedmrycgL0sIJZCBNAhFr3vYQZDZD');
+precacheFbPosts('+12063312167', 'EAACe39enYZCoBAOAZCZAa3puW6OpRr10kcBbSVKhUJnFQyCDDpz0hm1I010zsiLJ0uJIYtIFxdkAylr0BFqgPHxDvNvNqRoKM7ZCB1C86RoZAwlZAnZBs4yNUfo3dbQirDuJjhSBhO9BWQbvzFHPufedmrycgL0sIJZCBNAhFr3vYQZDZD');
